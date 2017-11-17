4e3ee67f970c9c7111c57bc90e9dc2e74ba93138
Eric Harrison <eric.h@rrison.io>
2017-11-17 00:05:58 -0500

    get a tile map loaded huzzah!

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,10 +1,15 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
 import Mushroom from '../sprites/Mushroom'
+import testMapJson from '../../assets/maps/test.json';
+// import mapSprite from '../../assets/sprites/testmapsprite.png';
 
 export default class extends Phaser.State {
   init () {}
-  preload () {}
+  preload () {
+    this.game.load.image('terrain', 'assets/sprites/testmapsprite.png');
+    this.game.load.tilemap('test', null, testMapJson, Phaser.Tilemap.TILED_JSON);
+  }
 
   create () {
     const bannerText = 'Phaser + ES6 + Webpack'
@@ -16,19 +21,25 @@ export default class extends Phaser.State {
     banner.smoothed = false
     banner.anchor.setTo(0.5)
 
-    this.mushroom = new Mushroom({
-      game: this.game,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
+    let map = this.game.add.tilemap('test');
+    map.addTilesetImage('terrain');
 
-    this.game.add.existing(this.mushroom)
+    let layer = map.createLayer('Tile Layer 1');
+    layer.scale.set(30);
+    layer.smoothed = false;
+    layer.resizeWorld();
+    // const map = new Phaser.Tilemap(this.game);
+    // const mapLayer = map.create('test', 100, 100, 10, 10);
+    // const newTile = new Phaser.Tile(mapLayer, 0, 0, 0, 10, 10)
+    // map.putTile(newTile, 0, 0);
+    // console.log(map.getTile(0, 0));
+
+    // console.log(Phaser.Tilemap.create());
   }
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+
     }
   }
 }
